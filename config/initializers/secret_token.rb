ccf9e01bbea5b425a685c727edd484d99384ac7e
Henryvw <Henryvw@gmail.com>
2013-11-27 22:17:20 +0100

    my initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lebenslauf::Application.config.secret_key_base = '15931645d450e1de65a5cf70e06fea3a549e67a4fe752c5569afa196e1744f803680dc27e34097bb630ad23de10b2933d5769c53111fe29e8dae94298e5cb2aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lebenslauf::Application.config.session_store :cookie_store, key: '_lebenslauf_session'
